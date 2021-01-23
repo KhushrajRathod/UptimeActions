429b6d06c624b6f00c08110a8bddd9241073eb72
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-23 19:26:27 +0530

    Add default monitors

---
+++ monitors.config.js
@@ -0,0 +1,89 @@
+module.exports = {
+    config: {
+        alertEndpoints: [
+            'https://monitor.khushrajrathod.com/api/slack'
+        ]
+    },
+    endpoints: [
+        {
+            name: 'Blog Rick',
+            url: 'https://blog.khushrajrathod.com/'
+        },
+        {
+            name: 'Click to a million',
+            url: 'https://ClickToAMillion.khushrajrathod.repl.co'
+        },
+        {
+            name: 'Discord bot',
+            url: 'https://LmaoBot.khushrajrathod.repl.co'
+        },
+        {
+            name: 'Hack Club Karma',
+            url: 'https://hackclubkarma.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'Important Bot',
+            url: 'https://ImportantDiscordBot.khushrajrathod.repl.co'
+        },
+        {
+            name: 'Important Hack Bot',
+            url: 'https://ImportantHackDiscordBot.khushrajrathod.repl.co'
+        },
+        {
+            name: 'Info Bot',
+            url: 'https://infobot.khushrajrathod.com'
+        },
+        {
+            name: 'Khushbot',
+            url: 'https://MyDiscordBot.khushrajrathod.repl.co'
+        },
+        {
+            name: 'Kuizzy',
+            url: 'https://kuizzy.khushrajrathod.com'
+        },
+        {
+            name: 'Lightning Delete',
+            url: 'https://lightning.khushrajrathod.com/ping'
+        },
+        {
+            name: 'Lightning Delete v1',
+            url: 'https://LightningDelete.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'Listen To Jia',
+            url: 'https://listentojia.khushrajrathod.com/ping'
+        },
+        {
+            name: 'OOCBot',
+            url: 'https://oocbot.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'Random scrapbook middleware',
+            url: 'https://HCRandomScrap.khushrajrathod.repl.co'
+        },
+        {
+            name: 'Slack Discord Bridge',
+            url: 'https://slackdiscordbridge.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'Telegram Discord Bridge',
+            url: 'https://TelegramDiscordBridge.khushrajrathod.repl.co'
+        },
+        {
+            name: 'The Overseer Extras',
+            url: 'https://The-Overseer-Extras.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'The Overseer Frontend',
+            url: 'https://the-overseer-frontend.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'Todo Slack App',
+            url: 'https://todoslackapp.khushrajrathod.repl.co/ping'
+        },
+        {
+            name: 'Web Chat',
+            url: 'https://DenoWebsocketChat-Auth.khushrajrathod.repl.co'
+        }
+    ]
+}
